655bc518d559eec9bd45b504ebc35b56c21bd2c0
fabrizioabreu <fabrizioabreu@live.com>
2023-05-16 09:17:40 -0300

    Back-end

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import fastfy from "fastify"
+import { PrismaClient } from "@prisma/client";
+
+const app = fastfy();
+const prisma = new PrismaClient()
+
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+
+  return users
+})
+
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log('🚀 HTTP server running on http://localhost:3333')
+})
