f374df427426ce032a423b5121f37df4923a59f3
fabrizioabreu <fabrizioabreu@live.com>
2023-05-20 22:27:34 -0300

    D-4 Back-end - Upload & Nova memória

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,20 @@ import 'dotenv/config'
 import fastfy from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastfy()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true, // todas URLs de front-end poderão acessar nosso back-end
@@ -16,11 +26,13 @@ app.register(jwt, {
 })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('🚀 HTTP server running on http://localhost:3333')
